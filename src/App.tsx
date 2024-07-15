95844e4083456912db72348f77ca8eaf71640cf7
Thomas Law <>
2024-07-15 17:14:56 +0800

    add require element to main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import './App.css'
 import { Difficulty } from "./class/Difficulty.tsx";
 import { CreativitySlider } from "./components/CreativityButton.tsx";
+import { RequirementElement } from "./class/RequirementElement.tsx";
+import { Divider } from "@mui/material";
 
 function App()
 {
@@ -15,22 +17,33 @@ function App()
     // Here will establish the data storage first
     const [difficulty, setDifficulty] = useState<number>( 0 );
     const [creativity, setCreativity] = useState<number>( 1 );
-    const [requireElement, setRequireElement] = useState<number>( 0 );
+    const [requireElement, setRequireElement] = useState<number>( 1 );
 
     // loop through the list and create the difficulty list
 
     return (
         <>
-            <h1> Total Score: { (difficulty * creativity).toFixed( 3 ) }</h1>
-            <h2> D: { difficulty.toFixed( 3 ) }</h2>
-            <h2> P: { creativity.toFixed( 3 ) }</h2>
-
-
+            <h2> Result(R) = Difficulty(D) x Presentation(P) x Deduction(M) x Required elements(Q) </h2>
+            <h2> Result = { difficulty.toFixed( 3 ) } x { creativity.toFixed( 3 ) } x 1.000
+                x { requireElement.toFixed( 3 ) } </h2>
+            <h2> Total Score = { (difficulty * creativity * requireElement).toFixed( 3 ) } </h2>
+            <Divider>
+                <h2>Difficulty</h2>
+            </Divider>
             <div key={ 'difficulty' }>
                 <Difficulty totalScore={ difficulty } setTotalScore={ setDifficulty }/>
             </div>
+            <Divider>
+                <h2>Creativity</h2>
+            </Divider> 
             <div key="creativity">
                 <CreativitySlider value={ creativity } setValue={ setCreativity }/>
+            </div>
+            <Divider>
+                <h2>Required Element</h2>
+            </Divider>
+            <div key="requireElement">
+                <RequirementElement totalScore={ requireElement } setScore={ setRequireElement }/>
             </div>
         </>
     )
